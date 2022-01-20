9865e4df7f5ed6c9e063fd8688a02fdaf0476382
AAKASH RAJ KASHYAP <araj190798@gmail.com>
2022-01-20 15:23:58 +0530

    for can_drive

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,13 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
-    // Write you code here
+    if (hasDrivingLiscence==false){
+        return "You cannot drive";
+    }
+    else{
+        if (isTired==true || isSober==false ){
+            return "You shouldn't drive"
+        }
+    }
+    return "You can drive"// Write you code here
 }
 
 module.exports = CanDrive;
